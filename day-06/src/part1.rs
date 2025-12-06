ddb7847134159a726bb60497fadef8e759496b45
Cor Bosman <cor@xs4all.net>
2025-12-06 11:20:17 +0100

    wip

--- day-06/src/part1.rs
+++ day-06/src/part1.rs
@@ -1,6 +1,13 @@
+use nom::{
+    character::complete::{anychar, i64 as parse_i64, multispace1, newline, space0, space1},
+    multi::{many1, separated_list1},
+    sequence::{preceded, terminated},
+    IResult, Parser,
+};
+
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
-    let (grid, ops) = read_input(input);
+    let (grid, ops) = parse_input(input);
 
     let result: i64 = transpose(grid).iter().zip(ops.iter())
         .map(|(row, op)| -> i64 {
@@ -15,29 +22,23 @@ pub fn process(input: &str) -> miette::Result<String> {
     Ok(result.to_string())
 }
 
-fn read_input(input: &str) -> (Vec<Vec<i64>>, Vec<char>) {
-    let lines: Vec<Vec<&str>> = input
-        .lines()
-        .map(|line| line.split_whitespace().collect())
-        .collect();
-
-    let grid: Vec<Vec<i64>> = lines[..lines.len() - 1]
-        .iter()
-        .map(|row| row.iter().map(|n| n.parse().unwrap()).collect())
-        .collect();
-
-    let ops: Vec<char> = lines.last().unwrap()
-        .iter()
-        .map(|s| s.chars().next().unwrap())
-        .collect();
-
-    (grid, ops)
-}
-
 fn transpose(grid: Vec<Vec<i64>>) -> Vec<Vec<i64>> {
     (0..grid[0].len())
         .map(|col| grid.iter().map(|row| row[col]).collect())
         .collect()
+}
+
+fn parse_input(input: &str) -> (Vec<Vec<i64>>, Vec<char>) {
+    let (_, result) = (many1(numbers), operators).parse(input).unwrap();
+    result
+}
+
+fn numbers(input: &str) -> IResult<&str, Vec<i64>> {
+    terminated(preceded(space0, separated_list1(space1, parse_i64)), newline).parse(input)
+}
+
+fn operators(input: &str) -> IResult<&str, Vec<char>> {
+    separated_list1(multispace1, anychar).parse(input)
 }
 
 #[cfg(test)]
